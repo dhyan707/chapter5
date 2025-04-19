cc7a53ca1d1ae8cf1198630c1987d285e35d41af
Dhyan <dhyanprem08@gmail.com>
2025-04-19 20:22:50 +0530

    first commit

---
+++ 03_MULTIPLE_IF.py
@@ -0,0 +1,12 @@
+a=int(input("enter the age: "))
+if(a%2==0):
+    print ("even")
+if(a>=18):
+    print ("ok")
+elif(a<0):
+    print ("invalid age")
+elif(a==0):
+    print ("new born baby")
+else:
+    print ("not ok")
+print ("end of program")
